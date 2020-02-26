b5f335256040f269e66e3950c91bc186ac038f6e
Filip Vušković <filip.vuskovic09@gmail.com>
2020-02-26 18:08:58 +0100

    ADD: home, playlist, profile; MOD: router adresses

--- include/services/router.php
+++ include/services/router.php
@@ -4,13 +4,13 @@ global  $uriParts, $page_load;
 // get page
 $requestUri = $_SERVER['REQUEST_URI'];
 $page = str_ireplace('/musicDB/', '', $requestUri);
-$uriParts = explode('/',$page);
+$uriParts = explode('/', $page);
 
 
 
 if ($page == "") {
 
-	$page_load = "bands";
+	$page_load = "home";
 
 } else {
 
@@ -20,6 +20,10 @@ if ($page == "") {
 
 			$page_load = "login";
 
+		} else if ($uriParts[0] == "bands") {
+
+			$page_load = "bands";
+			
 		} else {
 
             header('Location: /');
@@ -37,7 +41,7 @@ if ($page == "") {
 
 		} else if ($uriParts[0] == "user") {
 
-			$page_load = "playlist";
+			$page_load = "profile";
 
 		} else {
 
