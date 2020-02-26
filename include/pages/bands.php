7271ec38d19daf72930247625e864c1ea8d70179
Filip Vušković <filip.vuskovic09@gmail.com>
2020-02-26 23:20:01 +0100

    ADD: bands query test

--- include/pages/bands.php
+++ include/pages/bands.php
@@ -1,1 +1,13 @@
-bands
+<?php
+$query = $mysqli -> query("SELECT * FROM `bands` ORDER BY RAND() LIMIT 10");
+$result = $mysqli -> query($query);
+
+if ($result -> num_rows > 0) {
+    while($row = $result -> fetch_assoc()) {
+        echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. " " . $row["lastname"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+?>
