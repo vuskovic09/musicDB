222784214c9d523b328e44104196dea853b76910
Filip Vušković <filip.vuskovic09@gmail.com>
2020-03-11 00:33:05 +0100

    MOD: div styling + home headers

--- include/pages/home.php
+++ include/pages/home.php
@@ -11,9 +11,11 @@
 ?>
 
 <div class="home">
+
+    <h1 class="article-title">Bands</h1>
+
     <div class="container">
         <div class="home-bands">
-
             <?php foreach($dataBands as $row){ ?>
                 <div class="home-bands-card">
 
@@ -28,22 +30,27 @@
             <?php }?>
 
         </div>
+    </div>
 
-        <div class="home-albums">
+    <h1 class="article-title">Albums</h1>
 
-            <?php foreach($dataAlbums as $row){ ?>
-                <div class="home-albums-card">
+    <div class="container">
 
-                    <?php  if(isset($_SESSION['loggedUserName']) && !empty($_SESSION['loggedUserName'])) { ?>
-                    <span class="favorite" data-type="album" data-album="<?php echo $row['albumID'];?>"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg></span>
-                    <?php } ?>
+    <div class="home-albums">
 
-                    <a href="<?php echo $path . '/bands/' . $row['slug'] . '/' . $row['album_slug']; ?>" class="band-img" style="background-image: url(<?php echo $row['image'] ?>)"></a>
-                    <h3><?php echo($row['name']); ?></h3>
-                    <h3><?php echo($row['title']); ?></h3>
-                    <p><?php echo($row['year']); ?></p>
-                </div>
-            <?php }?>
+        <?php foreach($dataAlbums as $row){ ?>
+            <div class="home-albums-card">
+
+                <?php  if(isset($_SESSION['loggedUserName']) && !empty($_SESSION['loggedUserName'])) { ?>
+                <span class="favorite" data-type="album" data-album="<?php echo $row['albumID'];?>"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg></span>
+                <?php } ?>
+
+                <a href="<?php echo $path . '/bands/' . $row['slug'] . '/' . $row['album_slug']; ?>" class="band-img" style="background-image: url(<?php echo $row['image'] ?>)"></a>
+                <h3><?php echo($row['name']); ?></h3>
+                <h3><?php echo($row['title']); ?></h3>
+                <p><?php echo($row['year']); ?></p>
+            </div>
+        <?php }?>
 
         </div>
     </div>
