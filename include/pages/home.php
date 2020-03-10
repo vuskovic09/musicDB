640de15d254382fb4ca9da3c00fdbd33b96efbc0
Filip Vušković <filip.vuskovic09@gmail.com>
2020-03-10 14:41:19 +0100

    MOD: home temp class removal

--- include/pages/home.php
+++ include/pages/home.php
@@ -18,7 +18,7 @@
                 <div class="home-bands-card">
 
                     <?php  if(isset($_SESSION['loggedUserName']) && !empty($_SESSION['loggedUserName'])) { ?>
-                    <span class="favorite active" data-type="band" data-band="<?php echo $row['id'];?>"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg></span>
+                    <span class="favorite" data-type="band" data-band="<?php echo $row['id'];?>"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg></span>
                     <?php } ?>
 
                     <a href="<?php echo $path . '/bands/' . $row['slug']; ?>" class="band-img" style="background-image: url(<?php echo $row['photo'] ?>)"></a>
